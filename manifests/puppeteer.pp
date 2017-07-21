5d9e794d879223d6082a233fec867f1ef0216c16
harry yee <hyee1234@gmail.com>
2017-07-21 13:54:31 -0700

    Editted puppet file

--- manifests/puppeteer.pp
+++ manifests/puppeteer.pp
@@ -5,7 +5,7 @@ file { '/etc/hosts':
    owner => "root",
    group => "root",
    mode => "644",
-   source => "/root/boron_puppet/hosts",
+   source => "/root/boron_puppet/etc/hosts",
 }
 
 file { '/etc/hostname':
@@ -13,7 +13,7 @@ file { '/etc/hostname':
    owner => "root",
    group => "root",
    mode => "644",
-   source => "/root/boron_puppet/hostname",
+   source => "/root/boron_puppet/etc/hostname",
 }
 
 file { '/etc/resolv.conf':
@@ -21,16 +21,150 @@ file { '/etc/resolv.conf':
    owner => "root",
    group => "root",
    mode => "644",
-   source => "/root/boron_puppet/resolv.conf",
+   source => "/root/boron_puppet/etc/resolv.conf",
 }
 
 
-file { '/etc/dhcpd.conf':
+file { '/etc/fstab':
    ensure => "file",
    owner => "root",
    group => "root",
    mode => "644",
-   source => "/root/boron_puppet/dhcpd.conf",
+   source => "/root/boron_puppet/etc/fstab",
+}
+
+file { '/etc/gshadow':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/gshadow",
+}
+
+
+file { '/etc/group':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/group",
+}
+
+
+file { '/etc/passwd':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/passwd",
+}
+
+
+file { '/etc/profile.d/mpi.sh':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/profile.d/mpi.sh",
+}
+
+
+file { '/etc/shadow':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/shadow",
+}
+
+
+file { '/etc/slurm/slurm.conf':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/slurm/slurm.conf",
+}
+
+
+file { '/etc/sysconfig/iptables':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/sysconfig/iptables",
+}
+
+
+file { '/etc/sysconfig/network-scripts/ifcfg-em1':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/sysconfig/network-scripts/ifcfg-em1",
+}
+
+
+file { '/etc/sysconfig/network-scripts/ifcfg-em2':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/sysconfig/network-scripts/ifcfg-em2",
+}
+
+
+file { '/etc/sysconfig/network-scripts/ifcfg-ib0':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/sysconfig/network-scripts/ifcfg-ib0",
+}
+
+
+file { '/etc/vsftpd/vsftpd.conf':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/vsftpd/vsftpd.conf",
+}
+
+
+file { '/etc/xinetd.d/tftp':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/xinetd.d/tftp",
+}
+
+
+file { '/tftpboot/ftp/pub/anaconda-ks.cfg':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/tftpboot/ftp/pub/anaconda-ks.cfg",
+}
+
+
+file { '/tftpboot/pxelinux.cfg/default':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/tftpboot/pxelinux.cfg/default",
+}
+
+
+file { '/etc/dhcp/dhcpd.conf':
+   ensure => "file",
+   owner => "root",
+   group => "root",
+   mode => "644",
+   source => "/root/boron_puppet/etc/dhcp/dhcpd.conf",
 }
 
 $packages = ['ntp', 'mpi', 'slurm', 'munge', 'nfs', 'sudo']
